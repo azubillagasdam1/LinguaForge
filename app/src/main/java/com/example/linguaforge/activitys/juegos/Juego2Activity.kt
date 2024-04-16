860cc6210e0ee52e8f84671b473a9aae09dbfab6
azubillagasdam1 <azubillagasdam1@gmail.com>
2024-04-16 10:51:58 +0200

    Terminada logica con mas funcionalidad juego2

--- app/src/main/java/com/example/linguaforge/activitys/juegos/Juego2Activity.kt
+++ app/src/main/java/com/example/linguaforge/activitys/juegos/Juego2Activity.kt
@@ -1,16 +1,18 @@
     package com.example.linguaforge.activitys.juegos
 
+    import android.animation.ObjectAnimator
     import android.graphics.Color
     import android.media.MediaPlayer
     import android.os.Bundle
+    import android.os.Handler
+    import android.os.Looper
     import android.view.Gravity
     import android.view.View
+    import android.widget.ImageView
     import android.widget.LinearLayout
     import android.widget.TextView
-    import android.widget.Toast
     import androidx.activity.enableEdgeToEdge
     import androidx.appcompat.app.AppCompatActivity
-    import androidx.core.util.TypedValueCompat.dpToPx
     import androidx.core.view.ViewCompat
     import androidx.core.view.WindowInsetsCompat
     import com.example.linguaforge.R
@@ -18,13 +20,20 @@
     import kotlinx.coroutines.runBlocking
 
     class Juego2Activity : AppCompatActivity() {
+        private var marcador: TextView? = null
+        private var marcadorVidas: TextView? = null
+        private var corazonImageView: ImageView? = null
+        private final var MAXIMO_VIDAS: Int = 3
         private lateinit var clave: String
         private var palabras: List<List<String>>? = null
         private var textViewPalabra: TextView? = null
         private var jugadas: Int? = 0 // Variable para almacenar cantidad aciertos
         private var aciertos: Int? = 0 // Variable para almacenar cantidad aciertos
+        private var intentos: Int? = 0 // Variable para almacenar cantidad aciertos
         private var respuestaCorrecta: String? = null
         private var respuestarevuelta: String? = null
+        private var textViewReferencias = mutableListOf<TextView>()
+
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -37,6 +46,9 @@
             }
             clave = intent.getStringExtra("clave") ?: ""
             textViewPalabra = findViewById(R.id.palabraTextView)
+            marcador = findViewById(R.id.marcadorTextView)
+            marcadorVidas = findViewById(R.id.marcadorVidasTextView)
+            corazonImageView = findViewById(R.id.corazonImageView)
             obtenerPalabras()
 
             cargarPalabra()
@@ -46,6 +58,25 @@
 
         }
 
+
+        private fun actualizarMarcador() {
+            val totalPalabras = palabras?.size ?: 0
+            val totalAciertos = aciertos ?: 0
+            val totalIntentosRealizadas = intentos ?: 0
+
+
+            val totalFallos = totalIntentosRealizadas - totalAciertos
+
+
+            marcador?.text = "$jugadas / $totalPalabras"
+
+            val vidasRestantes = MAXIMO_VIDAS - totalFallos
+            marcadorVidas?.text = "X$vidasRestantes"
+
+            if (totalFallos >= MAXIMO_VIDAS || totalIntentosRealizadas >= totalPalabras) {
+                finalizar()
+            }
+        }
 
         private fun obtenerPalabras() = runBlocking {
             palabras = Utils.obtenerPalabrasPorClave(clave)?.shuffled()
@@ -89,7 +120,7 @@
                     textSize = 30f
                     gravity = Gravity.CENTER
                     setPadding(16, 16, 16, 16)
-                    setBackgroundResource(R.drawable.degradado_fondo)
+                    setBackgroundResource(R.drawable.style_bloque_letras)
                     // Establece la visibilidad del TextView según el índice
                     visibility =
                         if (i < numVisible) View.VISIBLE else View.INVISIBLE  // Usar INVISIBLE para mantener el espacio
@@ -113,41 +144,38 @@
         private fun generarCulebra(palabraDesordenada: String) {
             val contenedor = findViewById<LinearLayout>(R.id.culebraLinearLayout)
             contenedor.removeAllViews()  // Limpia vistas anteriores si es necesario
-            contenedor.orientation = LinearLayout.HORIZONTAL  // Asegúrate de que el contenedor es horizontal
+            textViewReferencias.clear()  // Limpia la lista de referencias de TextView
 
-            // Utiliza directamente la palabra desordenada recibida como parámetro
             for (i in palabraDesordenada.indices) {
                 val textView = TextView(this).apply {
-                    text = palabraDesordenada[i].toString().uppercase()  // Usa una letra desordenada
+                    text = palabraDesordenada[i].toString().uppercase()
                     textSize = 30f
-                    setPadding(16, 16, 16, 16)  // Ajusta el padding según necesites
+                    setPadding(16, 16, 16, 16)
                     gravity = Gravity.CENTER
-                    setBackgroundResource(R.drawable.degradado_fondo)  // Usa el drawable de degradado
+                    setBackgroundResource(R.drawable.style_bloque_letras)
                     layoutParams = LinearLayout.LayoutParams(
                         LinearLayout.LayoutParams.WRAP_CONTENT,
                         50.dpToPx()
                     ).also {
                         it.weight = 1f
-                        it.setMargins(
-                            4.dpToPx(),
-                            4.dpToPx(),
-                            4.dpToPx(),
-                            4.dpToPx()
-                        )  // Añade márgenes para la separación
+                        it.setMargins(4.dpToPx(), 4.dpToPx(), 4.dpToPx(), 4.dpToPx())
                     }
                     isClickable = true
                     setOnClickListener {
-                        // Aquí pasamos tanto la letra como el índice a comprobarJugada
                         comprobarJugada(text.toString().uppercase(), i)
+                        actualizarMarcador()
                     }
                 }
                 contenedor.addView(textView)
+                textViewReferencias.add(textView)  // Agrega la referencia a la lista
             }
         }
 
 
 
+
         private fun comprobarJugada(letra: String, index: Int) {
+            intentos = intentos!! + 1
             if (aciertos!! < respuestaCorrecta!!.length && letra.equals(
                     respuestaCorrecta!![aciertos!!].toString(),
                     ignoreCase = true
@@ -156,22 +184,121 @@
                 // Acción para cuando la letra es correcta
                 val mediaPlayer = MediaPlayer.create(this, R.raw.correct_sound)
                 mediaPlayer.start()
+                efectoAcierto(index)
                 aciertos = aciertos!! + 1  // Incrementa el contador de aciertos
                 generarHuecos(respuestaCorrecta!!, aciertos!!)
                 respuestarevuelta = respuestarevuelta!!.removeRange(index, index + 1)
                 generarCulebra(respuestarevuelta!!)
                 if ((aciertos!! < respuestaCorrecta!!.length).not()) {
-                    finish()
+                    aciertos = aciertos!! + 1
+                    recargarPartida()
+
+
                 }
             } else {
                 // Acción para cuando la letra es incorrecta
                 val mediaPlayer = MediaPlayer.create(this, R.raw.error_sound)
+
                 mediaPlayer.start()
+                efectoError(index)
+            }
+        }
+
+        private fun recargarPartida() {
+            jugadas = jugadas!! + 1
+            aciertos = 0
+            intentos = 0
+            efectoVerdeTemporal {
+                cargarPalabra()
+                generarHuecos(respuestaCorrecta!!, aciertos!!)
+            }
+        }
+
+        private fun efectoVerdeTemporal(onComplete: () -> Unit) {
+            val contenedor = findViewById<LinearLayout>(R.id.huecosLinearLayout)
+
+            for (i in 0 until contenedor.childCount) {
+                val letraContenedor = contenedor.getChildAt(i) as LinearLayout
+                val textView = letraContenedor.getChildAt(0) as TextView  // Asumiendo que el TextView es el primer hijo
+
+                // Cambia el fondo a verde permanentemente
+                textView.setBackgroundResource(R.drawable.style_bloque_acierto_letras)
+            }
+
+            // Usa un postDelayed para llamar al callback después de 500 ms sin cambiar de nuevo el fondo
+            contenedor.postDelayed({
+                onComplete()  // Ejecuta las acciones posteriores después del retardo
+            }, 500)
+        }
+
+
+        private fun efectoError(index: Int) {
+            if (index >= 0 && index < textViewReferencias.size) {
+                val textView = textViewReferencias[index]
+
+                // Cambia temporalmente el fondo a rojo
+                textView.setBackgroundResource(R.drawable.style_bloque_error_letras)
+
+                // Animación de "vibración" solo en el eje X
+                val shakeX = ObjectAnimator.ofFloat(textView, "translationX", 0f, 10f, -10f, 10f, -10f, 0f).apply {
+                    duration = 100  // Duración corta para una rápida "vibración"
+                }
+                shakeX.start()
+
+                // Handler para cambiar el fondo de nuevo al degradado después de una breve exposición del color rojo
+                Handler(Looper.getMainLooper()).postDelayed({
+                    // Cambia el fondo al degradado original
+                    textView.setBackgroundResource(R.drawable.style_bloque_letras)
+
+                    // Animación de fade-in para el degradado
+                    val fadeIn = ObjectAnimator.ofFloat(textView, "alpha", 0f, 1f).apply {
+                        duration = 500  // Duración de la animación de fade-in
+                    }
+                    fadeIn.start()
+                }, 100)  // Retardo para permitir que el color rojo sea visible
+            }
+        }
+
+        private fun efectoAcierto(index: Int) {
+            val contenedor = findViewById<LinearLayout>(R.id.huecosLinearLayout)
+            if (index >= 0 && index < contenedor.childCount) {
+                val letraContenedor = contenedor.getChildAt(index) as LinearLayout
+                val textView = letraContenedor.getChildAt(0) as TextView
+
+                // Cambia temporalmente el fondo a verde
+                textView.setBackgroundResource(R.drawable.style_bloque_acierto_letras)
+
+                // Animación de fade-out para el color verde
+                val fadeOut = ObjectAnimator.ofFloat(textView, "alpha", 1f, 0f).apply {
+                    duration = 1000  // Duración de la animación de fade-out
+                    start()
+                }
+
+                // Handler para cambiar el fondo de nuevo al degradado después de que el color verde sea visible
+                Handler(Looper.getMainLooper()).postDelayed({
+                    // Cambia el fondo al degradado original
+                    textView.setBackgroundResource(R.drawable.style_bloque_letras)
+
+                    // Animación de fade-in para el degradado
+                    val fadeIn = ObjectAnimator.ofFloat(textView, "alpha", 0f, 1f).apply {
+                        duration = 1000  // Duración de la animación de fade-in
+                        start()
+                    }
+                }, 1000)  // Retardo para permitir que el color verde sea visible
             }
         }
 
 
+
+
+
         private fun Int.dpToPx(): Int = (this * resources.displayMetrics.density).toInt()
 
+        private fun finalizar() {
+            finish()
+        }
 
+        fun irAtras(view: View) {
+            onBackPressed()
+        }
     }
